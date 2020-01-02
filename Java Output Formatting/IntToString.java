bac20938481d3f95f39b7521badff2dbeeb578a1
t2run <42409759+t2run@users.noreply.github.com>
2020-01-02 11:32:11 +0530

    Create IntToString.java

---
+++ Java Output Formatting/IntToString.java
@@ -0,0 +1,9 @@
+/*
+You are given an integer , you have to convert it into a string.
+
+Please complete the partially completed code in the editor. If your code successfully converts  into a string  the code will print "Good job". Otherwise it will print "Wrong answer".
+
+ can range between  to  inclusive.
+*/
+
+String s = String.valueOf(n);
